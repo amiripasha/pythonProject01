8d0281d021fe083c8171ca09f5ea02287d9fbf7d
mr.amiripasha <amiripasha.s@gmail.com>
2021-05-25 00:58:38 +0430

    ساخت یک فانکشن

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# print("hi world!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# todo must try more
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def jame(x, r):
+    return x + r
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(jame(2 ** 3, 2))
+
+print("Hi"+" "+"python"+" "+"Now!")
